02b8b612657841625a65bcf3bdc1e76cb95e9bb6
oscode04 <hellogaos@gmail.com>
2022-01-22 14:23:48 +0700

    Membuat repositori baru dan push semua code ke github

---
+++ src/views/Home.vue
@@ -0,0 +1,326 @@
+<template >
+  <v-container class="fill-height" fluid>
+    <!-- desktop -->
+    <v-row>
+      <v-col cols="12" sm="12" md="12" class="d-flex justify-center overflow-hidden" >
+        <!-- dekstop version -->
+        <!-- base card -->
+          <v-card height="590px" width="1240px" color="#C0CBEA" class="d-none d-sm-flex">
+            <v-container>
+              <v-row no-gutters>
+                <v-col >
+                  <!-- menu -->
+                  <v-container class="d-flex justify-start mb-n5 mt-5 ml-n7">
+                    <!-- <button v-for="menu in menus" :key="menu.id" :class="{add:menu.kondisi, kosong:menu.kondisi2}" @click="toggle(menu)">{{menu.label}}</button> -->
+                    <!-- <button :class="{add:menu.kondisi}" @click="toggle(menu)">Tambah Pasien2</button> -->
+                    <!-- <v-btn class="mr-2 add" plain>Tambah Pasien</v-btn>
+                    <v-btn class="mr-2">Daftar</v-btn>
+                    <v-btn class="mr-2">Cari Pasien</v-btn>
+                    <v-btn class="mr-2">Antrian</v-btn>
+                    <v-btn >Chekout Obat</v-btn> -->
+                  </v-container>
+                  <!-- end menu -->
+                </v-col> 
+              </v-row>  
+              <v-row justify="center">
+                <!-- content -->
+                <v-col cols="9" sm="12" md="9" lg="9" style="padding:0" class="cmargin">
+                    <v-card min-width="969px" height="462px" color="white" class="d-none d-sm-flex" ref="form">
+                      
+                      <!-- line 1 -->
+                      <v-container>
+                        <v-row style="margin-left:5px; margin-top:-5px">
+                          <!-- no record -->
+                            <v-col cols="2">
+                              <p style="margin-bottom:5px; margin-left:1px">No Record</p>
+                              <v-text-field solo dense v-model="norecord" label="No Record">
+                              </v-text-field>
+                            </v-col>
+                          <!-- end no record -->
+
+                          <!-- nama pasien -->
+                            <v-col cols="10">
+                              <p style="margin-bottom:5px; margin-left:1px">Nama Pasien</p>
+                              <v-text-field style="max-width:770px" solo dense v-model="nama" label="Nama Pasien">
+                              </v-text-field>
+                            </v-col>
+                          <!-- end nama pasien -->
+                      <!-- end line 1 -->
+
+                      <!-- line 2 -->
+                      <v-col cols="12" style="margin-left:1px; margin-top:-37px; margin-bottom:-5px">
+                        <p>Alamat</p>
+                      </v-col>
+                          <!-- RT -->
+                            <v-col cols="1" style="margin-top:-30px">
+                              <v-text-field solo dense v-model="rt" label="RT">
+                              </v-text-field>
+                            </v-col>
+                          <!-- end RT -->
+
+                          <!-- RW -->
+                            <v-col cols="1" style="margin-top:-30px">
+                              <v-text-field solo dense v-model="rw" label="RW">
+
+                              </v-text-field>
+                            </v-col>
+                          <!-- end RW -->
+
+                          <!-- Alamat -->
+                            <v-col cols="4" style="margin-top:-30px">
+                              <v-text-field solo dense v-model="alamat" label="Alamat">
+
+                              </v-text-field>
+                            </v-col>
+                          <!-- end Alamat -->
+
+                          <!-- Desa -->
+                            <v-col cols="2" style="margin-top:-30px">
+                              <v-text-field solo dense v-model="desa" label="Desa">
+
+                              </v-text-field>
+                            </v-col>
+                          <!-- end desa -->
+
+                          <!-- kecamaatan -->
+                            <v-col cols="2" style="margin-top:-30px">
+                              <v-text-field solo dense v-model="kecamatan" label="Kecamatan">
+
+                              </v-text-field>
+                            </v-col>
+                          <!-- end kecamatan -->
+
+                          <!-- kabupaten -->
+                            <v-col cols="2" style="margin-top:-30px">
+                              <v-text-field solo dense v-model="kabkota" label="Kab/Kota">
+
+                              </v-text-field>
+                            </v-col>
+                          <!-- end kabupaten -->
+                        </v-row>
+                      <!-- end line 2 -->
+                      
+                      <!-- line 3 -->
+                      <v-col cols="12" style="margin-left:5px; margin-top:-30px; margin-bottom:-5px">
+                        <p>Jenis Kelamin</p>
+                      </v-col>
+
+                        <!-- jenis kelamin -->
+                        <v-col cols="12" style="margin-left:13px; margin-top:-28px; margin-bottom:-5px">
+                          <v-radio-group v-model="jk" class="d-inline">
+                            <v-row >
+                              <v-col cols="2" style="padding:0">
+                                 <div class="d-inline">
+                                  <v-radio
+                                    label="Laki - laki"
+                                    value="laki-laki"
+                                    column
+                                  ></v-radio>
+                                </div>
+                              </v-col>
+
+                              <v-col cols="10" style="padding:0; margin-left:-20px">
+                                 <div class="d-inline">
+                                  <v-radio
+                                    label="Perempuan"
+                                    value="perempuan"
+                                  ></v-radio>
+                                </div>
+                              </v-col>
+                            </v-row>                           
+                          </v-radio-group>
+                        </v-col>
+                        <!-- end jenis kelamin -->
+                      <!-- end line 3 -->
+
+                      <!-- line 4 -->
+                      <v-row style="margin-left:5px; margin-top:-20px">
+                        <v-col cols="5">
+                          <p style="margin-bottom:5px; margin-left:1px">Nomor Telpon</p>
+                          <v-text-field solo dense v-model="telpon" :rules="[() => !!telpon || 'This field is required']" label="Nomor Telpon">
+                          </v-text-field>
+                        </v-col>
+                      </v-row>
+                      <!-- end line 4 -->
+
+                      <!-- line 5 -->
+                        <v-col cols="12" style="margin-left:5px; margin-top:-30px; margin-bottom:-5px">
+                          <p>Jenis Pasien</p>
+                        </v-col>
+                        <!-- jenis pasien -->
+                          <v-col cols="12" style="margin-left:13px; margin-top:-28px; margin-bottom:-5px">
+                            <v-radio-group  class="d-inline">
+                              <v-row >
+                                <v-col cols="2" style="padding:0">
+                                  <div class="d-inline">
+                                    <v-radio
+                                      label="UMUM"
+                                      value="umum"
+                                      column
+                                    ></v-radio>
+                                  </div>
+                                </v-col>
+
+                                <v-col cols="2" style="padding:0; margin-left:-20px">
+                                  <div class="d-inline">
+                                    <v-radio
+                                      label="BPJS"
+                                      value="bpjs"
+                                    ></v-radio>
+                                  </div>
+                                </v-col>
+
+                                <!-- nomor bpjs -->
+                                <v-col cols="8" style="padding:0; margin-left:-60px; margin-top:-7px">
+                                  <div class="d-inline">
+                                    <v-text-field solo dense label="Nomor BPJS" style="max-width:250px">
+                                    </v-text-field>
+                                  </div>
+                                </v-col>
+                                <!-- end nomor bpjs -->
+                              </v-row>                           
+                            </v-radio-group>
+                          </v-col>
+                        <!-- end jenis pasein -->
+                      <!-- end line 5 -->
+                      
+                      <!-- button -->
+                        <div class="d-flex flex-row-reverse" style="margin-top:-30px">
+                        <!-- save button -->
+                          <v-btn color="#FF515E" style="margin-left:15px" width="100px" @click="resetForm()">
+                            Batal
+                          </v-btn>
+                        <!-- end save button -->
+
+                        <!-- cancel button -->
+                        <v-btn color="#13C355" width="100px" @click="save()">
+                          Simpan
+                        </v-btn>
+                        <!-- end cancel button -->
+                        </div>
+                      <!-- end button -->
+                      </v-container>
+
+                    </v-card>   
+                </v-col>
+                <!-- end conten -->
+
+                <!-- profile -->
+                <v-col cols="3" sm="" md="3" lg="3" style="padding:0 0 0 80px;" class="mr-n5">
+                    <v-card max-width="211px" height="241px" color="red" class="d-none d-lg-block "> 
+
+                    </v-card>
+                </v-col>
+                <!-- end profile -->
+              </v-row>
+            </v-container>
+          </v-card>
+        <!-- end base card -->
+        <!-- end dekstop version -->
+
+        <!-- mobile version -->
+        <!-- base card -->
+          <v-card min-height="590px" min-width="500px" color="#C0CBEA" class="d-flex d-sm-none">
+
+          </v-card>
+        <!-- end base card -->
+        <!-- end mobile version -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+
+  data(){
+    return {
+      norecord : null,
+      erroMessage : '',
+      nama : '',
+      rt : null,
+      rw : null,
+      alamat : '',
+      desa : '',
+      kecamatan : '',
+      kabkota : '',
+      jk : '',
+      telpon : null,
+      formHasErrors : false
+    }
+    
+  },
+
+  computed : {
+    form () {
+      return {
+        noRecord : this.norecord,
+        namaPasien : this.nama,
+        alamat : [this.rt, this.rw, this.alamat, this.desa, this.kecamatan, this.kabkota],
+        jenisKelamin : this.jk,
+        telpon : this.telpon
+      }
+    }
+  },
+
+  watch : {
+    name () {
+      this.erroMessage = ''
+    }
+  },
+
+  methods :{
+      toggle: function(menu) {
+        menu.kondisi = !menu.kondisi
+      },
+
+      reset () {
+          this.norecord = null,
+          this.erroMessage = '',
+          this.nama = '',
+          this.rt = null,
+          this.rw = null,
+          this.alamat = '',
+          this.desa = '',
+          this.kecamatan = '',
+          this.kabkota = '',
+          this.jk = '',
+          this.telpon = null,
+          this.formHasErrors = false
+      },
+
+      resetForm () {
+        this.errorMessages = []
+        this.formHasErrors = true
+        this.reset()
+      },
+
+      save () {
+        this.formHasErrors = false
+
+        console.log(this.form)
+      },
+    },
+};
+
+</script>
+
+<style>
+.add {
+  background-color: red;
+  color: white;
+  border-radius: 7px;
+  margin-right: 10px;
+  padding: 5px 10px;
+}
+
+.kosong {
+  color: black;
+  margin-right: 10px;
+  padding: 5px 10px;
+}
+
+.cmargin {
+  margin-left: -15px;
+  margin-right: 15px;
+}
+</style>
---
+++ src/views/HomeAsli.vue
@@ -0,0 +1,328 @@
+<template>
+  <!-- base card -->
+  <v-card color="#C0CBEA" width="1240px" height="590px" class="base">
+    <!-- main content card -->
+    <div class="main-content">
+      <div class="title-input">
+        <!-- tittle -->
+        <div class="title">
+          <v-btn text class="add">Tambah Pasien</v-btn>
+          <v-btn text>Pendaftaran</v-btn>
+          <v-btn text>Cari Pasien</v-btn>
+          <v-btn text>Antrian Pasien</v-btn>
+          <v-btn text>Checkout Obat</v-btn>
+        </div>
+        <!-- end tittle -->
+        <!-- input card -->
+        <v-card color="#fff" width="969px" height="462px" class="card1">
+          <!-- <h2>Tambah Pasien Baru</h2> -->
+          <v-form>
+            <v-container>
+              <v-row class="input1">
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="2"
+                >
+                  <p class="mb-1 ml-1 ">No. Record</p>
+                  <v-text-field
+                    solo
+                    dense
+                    v-model="norecord"
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                  cols="12"
+                  sm="6"
+                  md="10"
+                >
+                  <p class="mb-1 ml-0">Nama Pasien</p>
+                  <v-text-field
+                    label="Nama"
+                    solo
+                    dense
+                    v-model="namapasien"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <div class="mt-n6">
+                <p class="mb-1 ml-1">Alamat</p>
+                <v-row class="input2">
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="1"
+                  >
+                    <v-text-field
+                      label="RT"
+                      solo
+                      dense
+                      v-model="rt"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="1"
+                  >
+                    <v-text-field
+                      label="RW"
+                      solo
+                      elevation="10"
+                      dense
+                      v-model="rw"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="4"
+                    md="4"
+                  >
+                    <v-text-field
+                      label="Alamat"
+                      solo
+                      dense
+                      v-model="alamat"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="2"
+                    md="2"
+                  >
+                    <v-text-field
+                      label="Desa"
+                      solo
+                      dense
+                      v-model="desa"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="2"
+                    md="2"
+                  >
+                    <v-text-field
+                      label="Kecamatan"
+                      solo
+                      dense
+                      v-model="kecamatan"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="2"
+                    md="2"
+                  >
+                    <v-text-field
+                      label="Kab/Kota"
+                      solo
+                      dense
+                      v-model="kabupatenkota"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </div>
+              
+              <div class="mt-n5">
+                <p class="mb-1 ml-1">Jenis Kelamin</p>
+                  <v-radio-group class="mt-n2">
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="2"
+                        class="mt-1"
+                      >
+                          <v-radio
+                            label="Laki-laki"
+                            value="Laki-laki"
+                            v-model="lk"
+                          ></v-radio>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="2"
+                        class="mt-1"
+                      >
+                          <v-radio
+                            label="Perempuan"
+                            value="Perempuan"
+                            v-model="pr"
+                          ></v-radio>
+                      </v-col>
+                    </v-row>
+                  </v-radio-group>
+              </div>
+
+              <div class="mt-n1">
+                <p class="mb-1 ml-1">Nomor Telpon</p>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="2"
+                    md="4"
+                  >
+                    <v-text-field
+                      label="Nomor Telpon"
+                      solo
+                      dense
+                      v-model="telpon"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </div>
+
+              <div class="mt-n5">
+                <p class="mb-1 ml-1">Jenis Pasien</p>
+                  <v-radio-group class="mt-n2">
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="2"
+                        class="mt-1"
+                      >
+                          <v-radio
+                            label="Umum"
+                            value="Umum"
+                            v-model="umum"
+                          ></v-radio>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="2"
+                        class="mt-1"
+                      >
+                          <v-radio
+                            label="BPJS"
+                            value="BPJS"
+                            v-model="bpjs"
+                          ></v-radio>
+                      </v-col>
+                      <v-col
+                        cols="12"
+                        sm="2"
+                        md="4"
+                        class="ml-n16 mt-n1"
+                      >
+                        <v-text-field
+                          label="Nomor BPJS"
+                          solo
+                          dense
+                          v-model="nomorbpjs"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-radio-group>
+              </div>
+
+              <div class="btn">
+                <div>
+                  <v-btn @click="save" color="#13C355" width="100px" height="35px" class="simpan-btn white--text heading-2" >
+                    <p>SIMPAN</p> 
+                  </v-btn>
+                </div>
+                <div>
+                  <v-btn @click="cancel" color="#FF515E" width="100px" height="35px" class="batal-btn white--text heading-2" >
+                    <p>BATAL</p> 
+                  </v-btn>
+                </div>
+              </div>
+    
+            </v-container>
+          </v-form>
+        </v-card>
+        <!-- end input card -->
+      </div>
+      
+      <!-- profile card -->
+      <div class="profile">
+        
+        <!-- biodata card -->
+        <v-card color="#fff" width="211px" height="241px" class="card2">
+          
+        </v-card>
+        <!-- end biodata card -->
+        
+        <!-- action button -->
+        <!-- profile button -->
+        <div>
+          <v-btn color="#13C355" width="211px" height="45px" class="card2 white--text heading-2" >
+            <h3>Profile</h3> 
+          </v-btn>
+        </div>
+        <!-- end profile button -->
+        <!-- logout button -->
+        <div class="button">
+          <v-btn color="#FF515E" width="211px" height="45px" class="card2 white--text">
+            <h3>Logout</h3>
+          </v-btn>
+        </div>
+        <!-- end logout button -->
+      </div>
+      <!-- end profile card -->
+    </div>
+    <!-- end main content card -->
+  </v-card>
+  <!-- end base card -->
+
+</template>
+<script>
+
+export default {
+
+  data(){
+    return {
+      norecord: "",
+      namapasien: "",
+      rt: "",
+      rw: "",
+      alamat: "",
+      desa:"",
+      kecamatan:"",
+      kabupatenkota: "",
+      lk:"",
+      pr:"",
+      telpon: "",
+      umum:"",
+      bpjs:"",
+      nomorbpjs:"",
+      data_pasien : [],
+      step : 0
+    }
+    
+    
+  },
+  methods: {
+    save() {
+      const x = this.norecord.push(this.data_pasien)
+      console.log(x)
+      // localStorage.namapasien = this.namapasien;
+      // localStorage.rt = this.rt;
+      // console.log(this.norecord, this.namapasien, this.rt, this.rw, this.alamat, this.desa);
+    },
+    
+    cancel() {
+      alert("oke")
+    },
+
+    change() {
+      if(this.step == 0){
+        alert('oke')
+      }
+    }
+
+  }
+};
+
+</script>
+
+<style>
+.border {
+    border-radius:50px;
+}
+</style>
